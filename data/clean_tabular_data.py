1767b8cc4e257b04cc7ae051dddbdeaa8d6265c6
Jazzmin Washington <102431019+Jazzmin-Washington@users.noreply.github.com>
2022-09-15 22:26:37 +0100

    Update clean_tabular_data.py

--- data/clean_tabular_data.py
+++ data/clean_tabular_data.py
@@ -24,9 +24,10 @@ class CleanTabularandImages():
     Removes the index column titled: Unnamed: 0 from the dataframe. 
     """
     def drop_columns(self):
-        print('Dropping unnecessary columns')
+        print('Dropping unnecessary columns \n')
         self.products.drop('Unnamed: 0', inplace=True, axis=1)
         self.products.drop('category', inplace=True,  axis=1)
+        self.images.drop('Unnamed: 0', inplace=True, axis=1)
         #self.products.drop('location', inplace=True,  axis=1)
         
     
@@ -38,7 +39,7 @@ class CleanTabularandImages():
         empty_fields = []
 
         unclean_data = self.products.copy()
-        print("Removing Missing Values")
+        print("Removing Missing Values \n")
         null_options = ['n/a', "N/A", "null", 'None','none']
         for n in null_options:
            if n in unclean_data:
@@ -52,7 +53,7 @@ class CleanTabularandImages():
     Converts price column into a float within the dataframe
     """
     def format_prices(self, max_price: int = 10000, min_price: int = 0.1):
-        print('Formatting price...')
+        print('Formatting price...\n')
         self.products['price'] = self.products['price'].str.replace(r'\£', '', regex = True)
         self.products['price'] = self.products['price'].str.replace(",", "", regex = True)
         self.products['price'] =  pd.to_numeric(self.products['price'], downcast="float")
@@ -68,7 +69,7 @@ class CleanTabularandImages():
     Removes the duplicates from the dataframe. 
     """
     def drop_duplicates(self):
-        print("Removing duplicates.")
+        print("Removing duplicates... \n")
         columns = ["product_name", "product_description", "location"]
         self.products.drop_duplicates(subset=columns, keep="first")
         
@@ -76,7 +77,7 @@ class CleanTabularandImages():
 
     ''' This function splits the categories into their own columns for future analysis'''
     def split_categories(self):
-        print('Splitting the categorical info ...')
+        print('Splitting the categorical info ...\n')
         for i in range(self.len_products):
             cat_list = ""
             cat_list = self.products['category'][i].split('/')
@@ -105,7 +106,7 @@ class CleanTabularandImages():
     ''' This function splits the city and region into separate columns and allows for the removal
         of entries that do not have a city and regions in the drop_columns function'''
     def split_location(self):
-        print('Splitting city and region information ...')
+        print('Splitting city and region information ...\n')
         self.len_products = len(self.products)
         for i in range(self.len_products):
             location = self.products['location'][i].split(',')
@@ -119,14 +120,16 @@ class CleanTabularandImages():
         self.products.drop('location',  axis=1)
          
     def clean_description(self):
+        print('Cleaning Description ...\n')
         self.products['product_description'].str.replace(r'/[^a-zA-Z0-9]/g', '', regex = True)
-        print(self.products['product_description'][0])    
+      
     
     '''This function matches the 'id' column within the Products.csv to the 
     'product_id' column within the Images.csv so the corresponding pictures can be matched to
     the products''' 
     
     def match_pic_id(self):
+        print('Matching product id with image identification...\n')
         self.image_list = []
         id_list = list(self.products['id'])
         for i in tqdm(range(len(id_list)), "Matching Product ID to Images:"):
@@ -140,8 +143,7 @@ class CleanTabularandImages():
         self.image_matches = pd.DataFrame.from_dict(self.image_list)
         print(self.image_matches.info())
 
-    ''' This function will add the image column for the list of images related to the product. 
-    The product_id in the products df was checked against the id in the image df '''
+    ''' This function will add the image column for the list of images related to the product. '''
     def merge_image_columns(self):
         self.products['images'] = self.image_matches['image_matches']
         print(self.products.info())
@@ -183,5 +185,8 @@ if __name__ == "__main__":
     #os.chdir('data/')
     Cleaner.products.to_csv(r'clean_fb_marketplace_products.csv')
     Cleaner.products.to_json(r'clean_fb_marketplace_products.json')
+    Cleaner.products.to_pickle(r'clean_fb_marketplace_products.pkl')
     print("Successfully cleaned and saved data")
   
+
+
