3cd50ab13af17031903f564fa914767f8f1e1a57
Jazzmin Washington <102431019+Jazzmin-Washington@users.noreply.github.com>
2022-09-26 14:57:19 +0100

    Update clean_images.py

--- data/clean_images.py
+++ data/clean_images.py
@@ -1,19 +1,42 @@
-#%% Clean and Resize Images
-from PIL import ImageFile
-from PIL import Image
-from alive_progress import alive_bar
+#%%
+# Create and Image Classifier
+import numpy as np
 import os
+import pandas as pd
+import torch
+import sklearn
+from numpy import asarray
+from PIL import Image
+from torchvision.transforms import ToTensor
+from torch import flatten
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from alive_progress import alive_bar
+from tqdm import tqdm
 
 
-class CleanImages():
-    def __init__(self, final_image_size: int = 512):
-        self.datapath = os.getcwd()
+
+class CreateImageClassifier():
+    def __init__(self, products, final_image_size: int = 512):
+        self.products = products
+        self.image_array = []
+        self.datapath = '/home/jazz/Documents/AiCore_Projects/Facebook-Marketplace-Ranking'
         self.final_image_size = final_image_size
-
+        self.image_list = pd.DataFrame()
+        self.image_array_full = []
+        
     def create_save_folder(self):
-        if not os.path.exists(f'{self.datapath}/data/clean_images'):
-            os.makedirs (f'{self.datapath}/data/clean_images' , exist_ok=True)
-            
+        if not os.path.exists(f'{self.datapath}/clean_images'):
+            os.makedirs (f'{self.datapath}/clean_images' , exist_ok=True)
+        
+    def compile_images(self):
+        self.second_image = self.products['image_2']
+        self.second_image.dropna(inplace = True)
+        self.images= list(self.second_image) + list(self.products['image_1'])
+        print(len(self.products['image_1']) + len(self.second_image))
+        self.image_list['image_id'] = self.images
+       
 
     def clean_image(self, img_name : str):
             # open image
@@ -37,23 +60,49 @@ class CleanImages():
 
             return final_img
 
-    def clean_all_images(self):
-        self.clean_image_save = f'{self.datapath}/data/clean_images'
-        os.chdir(f'{self.datapath}/data/images')
+    def process_all_images(self):
+        self.clean_image_save = f'{self.datapath}/clean_images'
+        os.chdir(f'{self.datapath}/images')
         with alive_bar(len(os.listdir())) as bar:
             for image in os.listdir():
-                if '.jpg' in image or '.jpeg' in image and image not in self.clean_image_save:
-                    img  = self. clean_image(image)
+                file_name = str(image).replace('clean_', "")
+                id = file_name.replace('.jpg', '')
+                if '.jpg' in image and image not in self.clean_image_save and id in self.images:
+                    img  = self.clean_image(image)
                     img.save(f'{self.clean_image_save}/clean_{image}')
+                    img_to_tensor = self.image_to_tensor(image)
+                    img_info = {'image_id': id,
+                                'array': img_to_tensor}
+                    self.image_array.append(img_info)
                     bar()
-        return None
+        self.images_w_array = pd.DataFrame(self.image_array)
+        self.images_w_array.to_csv(r'clean_image_array.csv')
 
-    def run_image_cleaner(self):
+        return None
+    
+    def image_to_tensor(self, img_name):   
+        image = Image.open(img_name)
+        t = ToTensor()
+        tensor_image = t(image)
+        flattened = torch.flatten(tensor_image) 
+        flat_numpy = flattened.numpy()
+        
+    
+        return flat_numpy
+    
+    def run_classifiers(self):
         self.create_save_folder()
-        self.clean_all_images()
+        self.compile_images()
+        self.process_all_images()
+        
+    
 
 if __name__ == "__main__":
-    cleaner = CleanImages()
-    print("Let's begin reformatiing the images...")
-    cleaner.run_image_cleaner()
-    print(f"Success! Reformatted Images were saved at: {cleaner.clean_image_save} ")
+    os.chdir('/home/jazz/Documents/AiCore_Projects/Facebook-Marketplace-Ranking/data')
+    data_path = os.getcwd()
+    products = pd.read_csv('clean_fb_marketplace_products.csv', lineterminator="\n")
+    classifier = CreateImageClassifier(products)
+    classifier.run_classifiers()
+# %%
+
+# %%
