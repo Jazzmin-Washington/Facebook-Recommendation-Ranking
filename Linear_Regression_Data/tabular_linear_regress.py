977ebeb17d926887fcf3860633a16fd5341d3749
Jazzmin Washington <102431019+Jazzmin-Washington@users.noreply.github.com>
2022-09-30 21:08:42 +0100

    Update tabular_linear_regress.py

--- Linear_Regression_Data/tabular_linear_regress.py
+++ Linear_Regression_Data/tabular_linear_regress.py
@@ -1,50 +1,49 @@
-#%%
-# Import Dependencies
-from re import X
-import numpy as np
-import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import Lasso
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.compose import make_column_transformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import make_column_transformer
+from sklearn import metrics
+
 
 class LinearRegressionMethod():
     def __init__(self):
-       self.products = ''
-    
-    def run_linear_regress(self):
-        os.chdir('/home/jazz/Documents/AiCore_Projects/Facebook-Marketplace-Ranking/data')
-   
-        # Read Data from file
-        print("Reading Data from Directory")
+        os.chdir('/home/jazzy/Documents/AiCore_Projects/Facebook-Marketplace-Ranking/data')
         self.products = pd.read_csv(f"clean_fb_marketplace_products.csv", lineterminator="\n")
-        self.data = self.products[['price']]
         
-        # Define matrix x and response vector y:
-        y = self.products.price
-        remove_price = self.products.drop('price', axis =1)
-        X = remove_price
 
-        #Split into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+    def load_data(self):
+        self.products.drop(['Unnamed: 0', 
+                            'id',
+                            'main_category',
+                            'subcategory_1',
+                            'cat_codes',
+                            'image_1',
+                            'image_2'],
+                             inplace=True,  axis=1)
+        self.products.info()
     
-        # Make column transform to transform text columns of feature matrix
-        column_trans = make_column_transformer(
-        (TfidfVectorizer(), "product_name"),
-        (TfidfVectorizer(), "product_description"),
-        (TfidfVectorizer(), "location"))
+    def run_data(self):
+        X = self.products.loc[:, ["product_name", "product_description", "County", "Region"]]
         
-       # define a pipeline
+        y = self.products['price']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+
+        column_trans =  make_column_transformer(
+            (TfidfVectorizer(), 'product_name'),
+            (TfidfVectorizer(), 'product_description'),
+            (TfidfVectorizer(), 'County'),
+            (TfidfVectorizer(), 'Region'))
+
+        
+        # define a pipeline
         pipe = make_pipeline(column_trans, LinearRegression())
         print(f'Making Pipeline : {pipe}')
 
@@ -57,9 +56,21 @@ class LinearRegressionMethod():
         print(f'Making predictions: {y_pred}')
 
         # Print Predictions
-        print(f'RMSE of model = {metrics.mean_squared_error(y_test, y_pred, squared = False)}')
-        print(f' R2 Score = {metrics.r2_score(y_test, y_pred)}')
+        print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+        print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+        print('Root Mean Squared Error:', metrics.r2_score(y_test, y_pred))
+
+    def run_linear_regress(self):
+        self.load_data()
+        self.run_data()
+        
+
 
 if __name__ == "__main__":
     LinearRegress = LinearRegressionMethod()
     LinearRegress.run_linear_regress()
+    
+
+
+# %%
+
