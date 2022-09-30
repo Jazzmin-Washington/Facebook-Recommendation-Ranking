c9058f6144fed45b94d1b506ccea2d92d656739a
Jazzmin Washington <102431019+Jazzmin-Washington@users.noreply.github.com>
2022-09-30 20:09:25 +0100

    Update clean_images.py

--- data/clean_images.py
+++ data/clean_images.py
@@ -1,108 +1,64 @@
-#%%
-# Create and Image Classifier
-import numpy as np
-import os
-import pandas as pd
-import torch
-import sklearn
-from numpy import asarray
+#%% Clean and Resize Images
+from PIL import ImageFile
 from PIL import Image
-from torchvision.transforms import ToTensor
-from torch import flatten
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
 from alive_progress import alive_bar
-from tqdm import tqdm
+import pandas as pd
+import os
 
 
-
-class CreateImageClassifier():
-    def __init__(self, products, final_image_size: int = 512):
-        self.products = products
-        self.image_array = []
-        self.datapath = '/home/jazz/Documents/AiCore_Projects/Facebook-Marketplace-Ranking'
+class CleanImages():
+    def __init__(self, final_image_size: int = 156):
+        os.chdir('/home/jazzy/Documents/AiCore_Projects/Facebook-Marketplace-Ranking/')
+        self.datapath = os.getcwd()
         self.final_image_size = final_image_size
-        self.image_list = pd.DataFrame()
-        self.image_array_full = []
         
+        
+
     def create_save_folder(self):
         if not os.path.exists(f'{self.datapath}/clean_images'):
             os.makedirs (f'{self.datapath}/clean_images' , exist_ok=True)
         
-    def compile_images(self):
-        self.second_image = self.products['image_2']
-        self.second_image.dropna(inplace = True)
-        self.images= list(self.second_image) + list(self.products['image_1'])
-        print(len(self.products['image_1']) + len(self.second_image))
-        self.image_list['image_id'] = self.images
-       
 
     def clean_image(self, img_name : str):
             # open image
-            img = Image.open(img_name)
+            with Image.open(img_name) as img:
 
-            # resize by finding the biggest side of the image and calculating ratio to resize by
-            max_side_length = max(img.size)
-            resize_ratio = self.final_image_size / max_side_length
-            img_width = int(img.size[0]*resize_ratio)
-            img_height = int(img.size[1]*resize_ratio)
-            img = img.resize((img_width, img_height))
+                # resize by finding the biggest side of the image and calculating ratio to resize by
+                max_side_length = max(img.size)
+                resize_ratio = self.final_image_size / max_side_length
+                img_width = int(img.size[0]*resize_ratio)
+                img_height = int(img.size[1]*resize_ratio)
+                img = img.resize((img_width, img_height))
 
-            # convert to rgb
-            img = img.convert("RGB")
+                # convert to rgb
+                img = img.convert("RGB")
 
-            # paste on black image
-            final_img = Image.new(mode="RGB", size=(
-                self.final_image_size, self.final_image_size))
-            final_img.paste(img, ((self.final_image_size - img_width) //
-                            2, (self.final_image_size - img_height)//2))
+                # paste on black image
+                final_img = Image.new(mode="RGB", size=(
+                    self.final_image_size, self.final_image_size))
+                final_img.paste(img, ((self.final_image_size - img_width) //
+                                2, (self.final_image_size - img_height)//2))
 
-            return final_img
+                return final_img
 
-    def process_all_images(self):
-        self.clean_image_save = f'{self.datapath}/clean_images'
-        os.chdir(f'{self.datapath}/images')
-        with alive_bar(len(os.listdir())) as bar:
-            for image in os.listdir():
-                file_name = str(image).replace('clean_', "")
-                id = file_name.replace('.jpg', '')
-                if '.jpg' in image and image not in self.clean_image_save and id in self.images:
-                    img  = self.clean_image(image)
-                    img.save(f'{self.clean_image_save}/clean_{image}')
-                    img_to_tensor = self.image_to_tensor(image)
-                    img_info = {'image_id': id,
-                                'array': img_to_tensor}
-                    self.image_array.append(img_info)
-                    bar()
-        self.images_w_array = pd.DataFrame(self.image_array)
-        self.images_w_array.to_csv(r'clean_image_array.csv')
+    def clean_all_images(self):
+            self.clean_image_save = f'{self.datapath}/clean_images'
+            os.chdir(f'{self.datapath}/images')
+            with alive_bar(len(os.listdir())) as bar:
+                for image in os.listdir():
+                    check = list(os.listdir(f'{self.clean_image_save}'))
+                    if '.jpg' in image and image not in check:
+                        img  = self. clean_image(image)
+                        img.save(f'{self.clean_image_save}/clean_{image}')
+                        bar()
+            return None
 
-        return None
-    
-    def image_to_tensor(self, img_name):   
-        image = Image.open(img_name)
-        t = ToTensor()
-        tensor_image = t(image)
-        flattened = torch.flatten(tensor_image) 
-        flat_numpy = flattened.numpy()
-        
-    
-        return flat_numpy
-    
-    def run_classifiers(self):
+    def run_image_cleaner(self):
         self.create_save_folder()
-        self.compile_images()
-        self.process_all_images()
-        
-    
+        self.clean_all_images()
 
 if __name__ == "__main__":
-    os.chdir('/home/jazz/Documents/AiCore_Projects/Facebook-Marketplace-Ranking/data')
-    data_path = os.getcwd()
-    products = pd.read_csv('clean_fb_marketplace_products.csv', lineterminator="\n")
-    classifier = CreateImageClassifier(products)
-    classifier.run_classifiers()
-# %%
-
-# %%
+    cleaner = CleanImages()
+    print("Let's begin reformatting the images...")
+    cleaner.run_image_cleaner()
+    print(f"Success! Reformatted Images were saved at: {cleaner.clean_image_save} ")
